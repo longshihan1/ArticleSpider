94a8c626a383a241d2d04f0a9fc5d339187b9a06
longhe <LONGHE001@huazhu.com>
2018-12-05 17:57:19 +0800

    添加正文提取功能

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -3,10 +3,12 @@ import scrapy
 import re
 from scrapy.http import Request
 from urllib import parse
-
+from ArticleSpider.spiders.CxExtractor import CxExtractor
+from ArticleSpider.items import JobBoleArticleItem
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
+
     allowed_domains = ['blog.jobbole.com']
     start_urls = ['http://blog.jobbole.com/all-posts/']
 
@@ -19,15 +21,11 @@ class JobboleSpider(scrapy.Spider):
             yield Request(url=parse.urljoin(response.url,post_url), meta={"front_image_url": parse.urljoin(response.url, image_url)}, callback=self.parse_detail)
 
         #提取下一页下载
-        """
         next_urls=response.css(".next.page-numbers::attr(href)").extract_first("")
         if next_urls:
             yield Request(url=parse.urljoin(response.url, next_urls), callback=self.parse)
-        """
     def parse_detail(self, response):
-        #提取文章的具体字段
-
-
+        article_item=JobBoleArticleItem()
         title = response.xpath("//div[@class='entry-header']/h1/text()").extract()[0]
         #通过CSS选择器
         title_css = response.css(".entry-header h1::text").extract()[0]
@@ -51,10 +49,15 @@ class JobboleSpider(scrapy.Spider):
             comment_nums=int(match_re.group(1))
         else:
             comment_nums=0
-        content = response.xpath("//div[@class='entry']").extract()[0]
+        #content = response.xpath("//div[@class='entry']").extract()[0]
         tags=response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
         tags=[element for element in tags if not element.strip().endswith("评论")]
         print(tags)
         front_image_url=response.meta.get("front_image_url", "")
         print(front_image_url)
+        # 提取文章的具体字段
+        cx = CxExtractor(threshold=186)
+        html_content = cx.filter_tags(response.text)
+        s_content = cx.getText(html_content)
+        print(s_content)
         pass
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ __author__ = 'bobby'
 from scrapy.cmdline import execute
 import sys
 import os
+
 #print(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 execute(["scrapy", "crawl", "jobbole"])
